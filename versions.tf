f03d2c47a5d2c674294cec031c68f91a9ff0e980
chrisghill <chris@massdriver.cloud>
2026-04-14 15:57:10 -0600

    add version constraints

--- versions.tf
+++ versions.tf
@@ -1,10 +1,14 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
     }
     massdriver = {
-      source = "massdriver-cloud/massdriver"
+      source  = "massdriver-cloud/massdriver"
+      version = ">= 1.0"
     }
   }
 }
